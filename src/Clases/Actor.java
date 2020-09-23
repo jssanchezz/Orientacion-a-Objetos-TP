76be33d6feae10ded8390a4a2b8daae2be2f198d
jssanchezz <jssanchezz20@gmail.com>
2020-09-23 20:04:12 -0300

    Atributos

--- src/Clases/Actor.java
+++ src/Clases/Actor.java
@@ -1,5 +1,34 @@
 package Clases;
 
 public abstract class Actor {
-  
+
+	protected int id;
+	protected Contacto contacto;
+	
+	//Constructor
+	
+	protected Actor(int id, Contacto contacto) {
+		this.id = id;
+		this.contacto = contacto;
+	}
+	
+	//Getters y setters
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Contacto getContacto() {
+		return contacto;
+	}
+
+	public void setContacto(Contacto contacto) {
+		this.contacto = contacto;
+	}
+	
+	
 }
--- src/Clases/RetiroLocal.java
+++ src/Clases/RetiroLocal.java
@@ -1,5 +1,27 @@
 package Clases;
 
-public class RetiroLocal {
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+public class RetiroLocal extends Entrega{
+	
+	private LocalTime horaEntrega;
+	
+	//Constructor
+
+	public RetiroLocal(int id, LocalDate fecha, boolean efectivo, LocalTime horaEntrega) {
+		super(id, fecha, efectivo);
+		this.horaEntrega = horaEntrega;
+	}
+	
+	//Getters y setters
+
+	public LocalTime getHoraEntrega() {
+		return horaEntrega;
+	}
+
+	public void setHoraEntrega(LocalTime horaEntrega) {
+		this.horaEntrega = horaEntrega;
+	}
+	
 }
