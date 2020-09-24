558de07e6c850251c064a8431ead2fb954bf5d57
Erio9 <59314062+Erio9@users.noreply.github.com>
2020-09-23 21:09:52 -0300

    Creacion de getters y setters

--- src/Clases/Ubicacion.java
+++ src/Clases/Ubicacion.java
@@ -2,4 +2,33 @@ package Clases;
 
 public class Ubicacion {
 
+	private double latitud;
+	private double longitud;
+
+	// Contructor
+
+	public Ubicacion(double latitud, double longitud) {
+		super();
+		this.latitud = latitud;
+		this.longitud = longitud;
+	}
+
+	// Getters y setters
+
+	public double getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(double latitud) {
+		this.latitud = latitud;
+	}
+
+	public double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+
 }
