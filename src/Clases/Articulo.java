1bf50cc26f9d40042103e12db0bc480219fc7f36
Erio9 <59314062+Erio9@users.noreply.github.com>
2020-09-26 05:30:01 -0300

    Envio Validar codigo de Barras
    Lo estuve probando con los productos de casa y funciona. Cualquier cosa que ayude a mejorarlo o que se me haya pasado avisenme

--- src/Clases/Articulo.java
+++ src/Clases/Articulo.java
@@ -47,19 +47,51 @@ public class Articulo {
 	public void setCodBarras(String codBarras) {
 		this.codBarras = codBarras;
 	}
+	
+	public boolean validarCodBarras(String codBarras) {
 
+		int numCodBarras[] = new int[13];
+		int par = 0, impar = 0, total = 0, codVerificador = 0;
+
+		for (int i = 0; i < codBarras.length(); i++) {
+			numCodBarras[i] = Character.getNumericValue(codBarras.charAt(i));//Primero convierto el "String" codBarras a valor numerico
+			if (i == codBarras.length() - 1) {
+				codVerificador = numCodBarras[i];//El ultimo digito del codigo de barras es considerado como el "codigo verificador"
+			} else if (i % 2 == 0) {
+				par += numCodBarras[i];
+			} else {
+				impar += numCodBarras[i];
+			}
+		}
+		impar *= 3;
+		total = par + impar;//Luego de sumar los totales se busca redondear el total hacia la decena mas cercana
+		if (total % 10 > 5) {
+			while (total % 10 != 0) {
+				total++;
+				codVerificador--;
+			}
+		}
+		else if(total % 10 < 5) {
+			while (total % 10 > 0) {
+				total--;
+				codVerificador--;//Para saber si el codigo de barras es correcto cuando el total redondee a la decena mas cercana 
+			}					//la diferencia entre ese redondeo y el codVerificador deberia ser 0
+		}
+		else {
+			codVerificador -= 5;
+		}
+		if (codVerificador == 0) {
+			return true;
+		} else
+			return false;
+	}
 
 	public double getPrecio() {
 		return precio;
 	}
 
-
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-	
-	
-	
-	
 
 }
