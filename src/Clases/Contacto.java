7b3c0864c71cc7686927615724de4dedd2d97958
rociomtrinidad <50055634+rociomtrinidad@users.noreply.github.com>
2020-09-26 05:20:26 -0300

    Update Contacto.java

--- src/Clases/Contacto.java
+++ src/Clases/Contacto.java
@@ -2,4 +2,46 @@ package Clases;
 
 public class Contacto {
 
+	private String email;
+	private String celular;
+	private Ubicacion ubicacion;
+	
+	//Constructor
+	
+	public Contacto(String email, String celular, Ubicacion ubicacion) {
+		super();
+		this.email = email;
+		this.celular = celular;
+		this.ubicacion = ubicacion;
+	}
+	
+	//Getters y setters
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getCelular() {
+		return celular;
+	}
+	
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	
+	public Ubicacion getUbicacion() {
+		return ubicacion;
+	}
+	
+	public void setUbicacion(Ubicacion ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	
+	
+	
 }
+
