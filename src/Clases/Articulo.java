ae82877b4ec9c195e9af07ad59054637d5a218de
rociomtrinidad <50055634+rociomtrinidad@users.noreply.github.com>
2020-09-26 14:28:28 -0300

    Update Articulo.java

--- src/Clases/Articulo.java
+++ src/Clases/Articulo.java
@@ -74,14 +74,17 @@ public class Articulo {
 		int sumaPares=0;
 		int sumaImpares=0;
 		int codigoVerif=0;
-		    for  (int i = 1; i< 12; i++){
-		        int numero= Integer.parseInt(cod.substring(i, i+1));
+		int numero=0;
+		int i=1;
+		    for  (i = 1; i< 13; i++){
+		        numero= Integer.parseInt(cod.substring(i, i+1));
 		        if (i%2==0){
 		            sumaPares=sumaPares+numero;
 		        }
 		        else{
-		            sumaImpares=sumaImpares+numero;
+		            sumaImpares=sumaImpares+numero;    
 		        }
+		        
 		    }
 		    numero= Integer.parseInt(cod.substring(i, i+1));
 		    int resImpares = sumaImpares*3;
