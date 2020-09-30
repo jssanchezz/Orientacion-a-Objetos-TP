bf618b130aabe2e2b43ce48a6933736ec2009242
jssanchezz <jssanchezz20@gmail.com>
2020-09-30 16:34:59 -0300

    Update

--- src/Test/TestAlmacenGranate.java
+++ src/Test/TestAlmacenGranate.java
@@ -4,20 +4,10 @@ import Clases.Articulo;
 import Clases.Carrito;
 import Clases.Cliente;
 import Clases.Comercio;
-import Clases.Turno;
 import Clases.Contacto;
-import Clases.DiaRetiro;
-import Clases.Entrega;
-import Clases.ItemCarrito;
-import Clases.RetiroLocal;
-import Clases.Entrega;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
-
 
 
 public class TestAlmacenGranate {
@@ -30,23 +20,24 @@ public class TestAlmacenGranate {
 			Articulo articulo1 = null;
 			Articulo articulo2 = null;
 			Cliente cliente1 = null;
+			
 			//Articulo articulo2 = new Articulo("Sabonis sabor", "6937733560324", 5.25);
 			// -------------------------------------------------------------
 			// Se Añade un nuevo Comercio:
 			// -------------------------------------------------------------
+			
+			System.out.println("\n---->Escenario 1: Nuevo comercio con [cuit = 30610252334]<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 1: Nuevo comercio con [cuit = 30610252334]<----");
-				System.out.print("\n");
 				comercio1 = new Comercio(2, contacto1, "Super Pepe", 30610252334l, 22.52, 14.55, 2, 6, 25);
 				System.out.println(comercio1);
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
+			
+			System.out.println("\n---->Escenario 2: Nuevo comercio con [cuit = 3012354825]<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 2: Nuevo comercio con [cuit = 3012354825]<----");
-				System.out.print("\n");
 				comercio1 = new Comercio(2, null, "Super Pedro", 3012354825l, 22.52, 14.55, 2, 6, 25);
 				System.out.println(comercio1);
 			} catch (Exception e) {
@@ -54,22 +45,23 @@ public class TestAlmacenGranate {
 			} finally {
 				System.out.println("\n\t---------Fin del caso de uso para crear un nuevo comercio---------");
 			}
+			
 			// -------------------------------------------------------------
-			// Se Aniade un nuevo articulo:
+			// Se Añade un nuevo articulo:
 			// -------------------------------------------------------------
+			
+			System.out.println("\n---->Escenario 1: Nuevo Articulo con [codigo de barras = 7898024394181]<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 1: Nuevo Articulo con [codigo de barras = 7898024394181]<----");
-				System.out.print("\n");
 				articulo1 = new Articulo("Nutella", "7898024394181", 140.5);
 				System.out.println(articulo1);
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
-			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 2: Nuevo Articulo con [codigo de barras = 4564128481231]<----");
-				System.out.print("\n");
+			
+			System.out.println("\n---->Escenario 2: Nuevo Articulo con [codigo de barras = 4564128481231]<----\n");
+			
+			try {			
 				articulo1 = new Articulo("Nutella", "4564128481231", 140.5);
 				System.out.println(articulo1);
 			} catch (Exception e) {
@@ -77,24 +69,23 @@ public class TestAlmacenGranate {
 			} finally {
 				System.out.println("\n\t---------Fin del caso de uso para crear un articulo---------");
 			}
-			ItemCarrito itemCarrito1 = new ItemCarrito(articulo1, 1);
+			
 			// -------------------------------------------------------------
 			// Se Aniade un nuevo Articulo a la lista de articulos del Comercio:
 			// -------------------------------------------------------------
+			
+			System.out.println("\n---->Escenario 1: Se agrega un Articulo a la lstArticulo de comercio<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 1: Se agrega un Articulo a la lstArticulo de comercio<----");
-				System.out.print("\n");
 				comercio1.agregarArticulo(articulo1);
 				System.out.println(comercio1);
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
+			
+			System.out.println("\n---->Escenario 2: Se agrega un Articulo a la lstArticulo de comercio que ya existe<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println(
-						"---->Escenario 2: Se agrega un Articulo a la lstArticulo de comercio que ya existe<----");
-				System.out.print("\n");
 				comercio1.agregarArticulo(articulo1);
 				System.out.println(comercio1);
 			} catch (Exception e) {
@@ -106,19 +97,19 @@ public class TestAlmacenGranate {
 			// -------------------------------------------------------------
 			// Se Aniade un nuevo Cliente:
 			// -------------------------------------------------------------
+			
+			System.out.println("\n---->Escenario 1: Nuevo Cliente con [dni = 22354125]<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 1: Nuevo Cliente con [dni = 22354125]<----");
-				System.out.print("\n");
 				cliente1 = new Cliente(2, null, "Lopez", "Fernando", 22354125l);
 				System.out.println(cliente1);
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
-			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 2: Nuevo Cliente con [dni = 535412]<----");
-				System.out.print("\n");
+			
+			System.out.println("\n---->Escenario 2: Nuevo Cliente con [dni = 535412]<----\n");
+			
+			try {				
 				cliente1 = new Cliente(2, contacto1, "Fernando", "Lopez", 535412l);
 				System.out.println(cliente1);
 			} catch (Exception e) {
@@ -129,11 +120,12 @@ public class TestAlmacenGranate {
 			// -------------------------------------------------------------
 			// Se agrega items al carrito:
 			// -------------------------------------------------------------
+			
+			System.out.println("\n---->Escenario 1: Se agrega 1 item al carrito<----\n");
+			
 			Carrito carrito1 = new Carrito(LocalDate.of(2020, 8, 20), LocalTime.of(10, 15), 22.45, cliente1, null);
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 1: Se agrega 1 item al carrito<----");
-				System.out.print("\n");
 				System.out.println(carrito1);
 				carrito1.agregar(articulo1, 3);
 				System.out.println(carrito1);
@@ -145,31 +137,30 @@ public class TestAlmacenGranate {
 			// -------------------------------------------------------------
 			// Se agrega un carrito al lstcarrito de Comercio:
 			// -------------------------------------------------------------
+			
+			System.out.println("\n---->Escenario 1: Se agrega un carrito a la lstCarrito de comercio<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 1: Se agrega un carrito a la lstCarrito de comercio<----");
-				System.out.print("\n");
 				comercio1.agregarCarrito(carrito1);
 				System.out.println(comercio1);
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
+			
+			System.out.println("\n---->Escenario 2: Se agrega un carrito a la lstCarrito de comercio que ya existe<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println(
-						"---->Escenario 2: Se agrega un carrito a la lstCarrito de comercio que ya existe<----");
-				System.out.print("\n");
 				comercio1.agregarCarrito(carrito1);
 				System.out.println(comercio1);
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
+			
+			System.out.println("\n---->Escenario 3: Se agrega un carrito a la lstCarrito de comercio que tiene el carrito cerrado<----\n");
+			
 			carrito1.setCerrado(true);
+			
 			try {
-				System.out.print("\n");
-				System.out.println(
-						"---->Escenario 3: Se agrega un carrito a la lstCarrito de comercio que tiene el carrito cerrado<----");
-				System.out.print("\n");
 				comercio1.agregarCarrito(carrito1);
 				System.out.println(comercio1);
 			} catch (Exception e) {
@@ -181,20 +172,20 @@ public class TestAlmacenGranate {
 			// -------------------------------------------------------------
 			// Se saca items del carrito:
 			// -------------------------------------------------------------
+			
+			System.out.println("\n---->Escenario 1: Sacar Items 3 unidad de un Articulo del carrito<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 1: Sacar Items 3 unidad de un Articulo del carrito<----");
-				System.out.print("\n");
 				System.out.println(carrito1);
 				carrito1.sacar(articulo1, 3);
 				System.out.println(carrito1);
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
+			
+			System.out.println("\n---->Escenario 2: Sacar Items 3 unidades de un Articulo del carrito<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 2: Sacar Items 3 unidades de un Articulo del carrito<----");
-				System.out.print("\n");
 				carrito1.sacar(articulo1, 3);
 				System.out.println(carrito1);
 			} catch (Exception e) {
@@ -205,20 +196,19 @@ public class TestAlmacenGranate {
 			// -------------------------------------------------------------
 			// Se elimina un nuevo Articulo de la lst Articulo de Comercio:
 			// -------------------------------------------------------------
+			
+			System.out.println("\n---->Escenario 1: Se elimina un Articulo a la lstArticulo de comercio<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 1: Se elimina un Articulo a la lstArticulo de comercio<----");
-				System.out.print("\n");
 				comercio1.eliminarArticulo(articulo1);
 				System.out.println(comercio1);
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
+			
+			System.out.println("\n---->Escenario 2: Se intenta eliminar un Articulo a la lstArticulo del comercio que ya fue eliminado<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println(
-						"---->Escenario 2: Se intenta eliminar un Articulo a la lstArticulo del comercio que ya fue eliminado<----");
-				System.out.print("\n");
 				comercio1.eliminarArticulo(articulo1);
 				System.out.println(comercio1);
 			} catch (Exception e) {
@@ -230,21 +220,20 @@ public class TestAlmacenGranate {
 			// -------------------------------------------------------------
 			// Se elimina un carrito al lstcarrito de Comercio:
 			// -------------------------------------------------------------
+			
+			System.out.println("\n---->Escenario 1: Se elimina un carrito a la lstCarrito de comercio<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println("---->Escenario 1: Se elimina un carrito a la lstCarrito de comercio<----");
-				System.out.print("\n");
 				comercio1.eliminarCarrito(carrito1);
 				comercio1.eliminarCarrito(carrito1);
 				System.out.println(comercio1);
 			} catch (Exception e) {
 				System.out.println("Exception: " + e.getMessage());
 			}
+			
+			System.out.println("\n---->Escenario 2: Se elimina un carrito a la lstCarrito de comercio que ya fue eliminado<----\n");
+			
 			try {
-				System.out.print("\n");
-				System.out.println(
-						"---->Escenario 2: Se elimina un carrito a la lstCarrito de comercio que ya fue eliminado<----");
-				System.out.print("\n");
 				comercio1.eliminarCarrito(carrito1);
 				System.out.println(comercio1);
 			} catch (Exception e) {
